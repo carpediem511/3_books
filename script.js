39ea12ff67b8e60d8e917eb6d7c2fafc4410f5d9
carpediem511 <vampire.0514@gmail.com>
2022-08-31 19:56:22 +0300

    готово js

--- script.js
+++ script.js
@@ -1,165 +1,148 @@
 
-const books = [
-    {
-        id: 1,
-        title: "Гарри Поттер и философский камень",
-        authors: "Джоан Роулинг",
-        year: "1997",
-        image: "/book1.jpg"
-    },
+// ввести счётчик для книг, чтобы в id записывались значения
 
-    {
-        id: 2,
-        title: "Гарри Поттер и тайная комната",
-        authors: "Джоан Роулинг",
-        year: "1998",
-        image: "/book2.jpg"
-    },
+let booksCounter = 1;
 
-    {
-        id: 3,
-        title: "Гарри Поттер и узник Азкабана",
-        authors: "Джоан Роулинг",
-        year: "1999",
-        image: "/book3.jpg"
-    },
+const books = [ //создать массив из книг
+  {
+    // id: 1,
+    id: booksCounter++,
+    title: "Гарри Поттер и философский камень",
+    authors: "Джоан Роулинг",
+    year: "1997",
+    image: "./book1.jpg"
+  },
 
-    {
-        id: 4,
-        title: "Гарри Поттер и кубок огня",
-        authors: "Джоан Роулинг",
-        year: "2000",
-        image: "/book4.jpg"
-    }
+  {
+    // id: 2,
+    id: booksCounter++,
+    title: "Гарри Поттер и тайная комната",
+    authors: "Джоан Роулинг",
+    year: "1998",
+    image: "./book2.jpg"
+  },
+
+  {
+    // id: 3,
+    id: booksCounter++,
+    title: "Гарри Поттер и узник Азкабана",
+    authors: "Джоан Роулинг",
+    year: "1999",
+    image: "./book3.jpg"
+  },
+
+  {
+    // id: 4,
+    id: booksCounter++,
+    title: "Гарри Поттер и кубок огня",
+    authors: "Джоан Роулинг",
+    year: "2000",
+    image: "./book4.jpg"
+  }
 ]
-   
-const list2 = document.getElementById("list")
 
-function renderBooks() {
-    list2.innerHTML = ""
-    books.forEach((book) => {
-        list2.innerHTML += `
-            <div class="list"> 
-                <div class="book">
-                    <div class="book-image"><img src="${book.image}" class="book-image-style"/></div>
-                    <div class="book-title">${book.title}</div>
-                    <div class="book-year">${book.year}</div>
-                    <div class="book-author">${book.authors}</div>
-                    <div class="book-buttons">
-                        <button class="book-button">Изменить</button>
-                        <button onclick="deleteBook(${book.id})" class="book-button">Удалить</button>
-                    </div>
-                </div>
-            </div>
-       `
-    })
-}
+const list2 = document.getElementById("list") //найти в HTML контейнер с книгами
 
-
-function deleteBook(id) {
-    const bookDel = books.find((findBook) => {
-
-        return findBook.id === id
-
-        
-    })
-
-   const bookIndex = books.indexOf(bookDel)
-   console.log(bookIndex)
-
-   books.splice(bookIndex, 1)
-
-   renderBooks()
-}
-
-
-function addBook() {
-    
-    const bookProperties = document.getElementById("bookPropertiesId")
-
-    bookProperties.innerHTML += `
-    <div class="propertiesNewBook" id="propertiesNewBookId">
-
-    <input id="bookName" placeholder="Имя книги"></input>
-
-    <input id="bookAuthor" placeholder="Автор книги"></input>
-
-    <input id="bookYear" placeholder="Год публикации книги"></input>
-
-    <input id="bookImage" placeholder="Ссылка на изображение"></input>
-
-    <button id="bookSave" onclick="saveBook()">Сохранить</button></input>
-
+function renderBooks() { // ввести функцию - визуализировать книги
+  list2.innerHTML = "" // пока в контйнере пусто
+  books.forEach((book) => { // добавляю в html контейнер, в котором описаны свойства книг
+    list2.innerHTML += ` 
+    <div class="list"> 
+      <div class="book">
+        <div class="book-image"><img src="${book.image}" class="book-image-style"/></div>
+        <div class="book-title">${book.title}</div>
+        <div class="book-year">${book.year}</div>
+        <div class="book-author">${book.authors}</div>
+        <div class="book-buttons">
+          <button class="book-button">Изменить</button>
+          <button onclick="deleteBook(${book.id})" class="book-button">Удалить</button>
+        </div>
+      </div>
     </div>
+    `
+  })
+}
 
- `
+function deleteBook(id) { //создаю функцию для удаления книги, кнопка "Удалить"
+  const bookDel = books.find((findBook) => {
+    return findBook.id === id //найти книгу по id
+  })
+
+  const bookIndex = books.indexOf(bookDel) //присвоить переменной индексы книг из массива
+  
+  books.splice(bookIndex, 1)
+
+  renderBooks()
+}
+
+function addBook() { //Функция "добавить книгу"
+
+  const bookProperties = document.getElementById("bookPropertiesId") //найти контейнер в html
+
+  // Дальше проверка: если элемент не пустой, то нужно выйти из функции заранее при помощи "return"
+  if (bookProperties.childElementCount) return;
+
+  bookProperties.innerHTML = `
+  <div class="propertiesNewBook" id="propertiesNewBookId">
+
+    <input id="bookName" placeholder="Имя книги">
+
+    <input id="bookAuthor" placeholder="Автор книги">
+
+    <input id="bookYear" placeholder="Год публикации книги">
+
+    <input id="bookImage" placeholder="Ссылка на изображение">
+
+    <button id="bookSave" onclick="saveBook()">Сохранить</button>
+
+  </div>
+  `
 }  
 
-const addBookButton = document.getElementById("addBookButton")
-addBookButton.addEventListener("click", addBook)
-addBookButton.addEventListener("click", showForm)
+const addBookButton = document.getElementById("addBookButton") //найти кнопку "Добавить книгу" в html
+addBookButton.addEventListener("click", addBook) //привязать к кнопке функцию, по клику на неё
 
+function showForm() { //создать функцию "показать форму"
+  let openForm = document.getElementById("bookPropertiesId") //найти контейнер в html
+  let buttonClick = document.getElementById("bookSave") //найти кнопку "сохранить книгу"
 
-let isOpen = false
+  let isOpen = false //ввести переменную, которая говорит, что если форма открыта, то это неверно
 
-function showForm() {
+  if (buttonClick) { // если нажали на кнопку "сохранить"
+    if (isOpen) { // если форма уже была открыта, значит нажали еще раз - закрываем
+      openForm.style.display = "none" //скрываем контейнер
+      isOpen = false // установить закрытым
 
-   let openForm = document.getElementById("bookPropertiesId")
-   let buttonClick = document.getElementById("bookSave")
-
- 
-   if (buttonClick) { // если у нас нажали на первый элемент 
-		if (isOpen) { // если он уже был открыт, значит, нажали еще раз - закрываем
-
-            openForm.style.display = "none"
-            isOpen = false // установить закрытым
-   
-		} else {
-		
-            openForm.style.display = "flex"
-        	isOpen = true // установить открытым
-        
-		}
- 	}
-}
-  
-
-function clearField() {
-    document.getElementById("bookName").value = ""
-    document.getElementById("bookAuthor").value = ""
-    document.getElementById("bookYear").value = ""
-    document.getElementById("bookImage").value = ""
-}
-
-function saveBook () {
-
-    const bookNameValue = document.getElementById("bookName").value
-    const bookAuthorValue = document.getElementById("bookAuthor").value
-    const bookYearValue = document.getElementById("bookYear").value
-    const bookImageValue = document.getElementById("bookImage").value
-
-    const book = {
-        title: bookNameValue,
-        authors: bookAuthorValue,
-        year: bookYearValue,
-        image: bookImageValue
+    } else { // иначе
+      openForm.style.display = ""
+      isOpen = true // установить открытым
     }
+  }
+}
 
-    books.push(book)
-    renderBooks()
-    clearField()
-    hideField()
-    
+function saveBook () { //ввести функцию "сохранить книгу"
+  const bookNameValue = document.getElementById("bookName").value //получить значение из поля "имя"
+  const bookAuthorValue = document.getElementById("bookAuthor").value //получить значение из поля "автор"
+  const bookYearValue = document.getElementById("bookYear").value //получить значение из поля "год"
+  const bookImageValue = document.getElementById("bookImage").value //получение значение из поля "ссылка"
+
+  const book = { //ввести переменную, где будут храниться все эти значения value
+
+    id: booksCounter++,
+    title: bookNameValue,
+    authors: bookAuthorValue,
+    year: bookYearValue,
+    image: bookImageValue
+  }
+
+  books.push(book) //добавить книгу
+  renderBooks() //отобразить книгу
+  hideField() //скрыть форму
+}
+
+function hideField() { //ввести функцию "скрыть форму"
+  const hideForm = document.getElementById("propertiesNewBookId") //найти контейнер, который надо скрыть
+  hideForm.remove() // удалить форму
 }
 
 renderBooks()
-
-    
-    
-
-
-
-    
- 
-
- 
